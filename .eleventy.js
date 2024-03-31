3d3198d659df614efe653f89cc8ee12266b61495
sans <sannadgtl@gmail.com>
2024-03-31 16:37:39 +0100

    new footer and hero section

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images/"); 
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
+    eleventyConfig.addPassthroughCopy("./src/style.css"); 
+    // eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
 
     return {
       dir: {
