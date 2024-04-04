e115d532f644c63e2dd32417125f17def0544720
sans <sannadgtl@gmail.com>
2024-04-04 16:52:43 +0100

    tip calculator project

--- src/index.js
+++ src/index.js
@@ -28,3 +28,23 @@ function submitOrder() {
 
 const button = document.querySelector("#submit-btn");
 button.addEventListener("click", submitOrder);  
+
+// tip calculator
+function calculateTotal() {
+    let billAmount = parseFloat(document.getElementById("billAmount").value); 
+    let tipPercentage = parseFloat(document.getElementById("tipPercentage").value)/100;
+
+    if (billAmount >= 0.01 && tipPercentage >=0 && tipPercentage <= 1) {
+        let totalTip = billAmount * tipPercentage; 
+        let totalAmount = billAmount + totalTip; 
+
+        document.getElementById("totalBill").textContent = `Your total bill (£${billAmount}) with ${tipPercentage * 100}% tip is: £${totalAmount.toFixed(2)}`; 
+        document.getElementById("billAmount").value = ""; 
+        document.getElementById("tipPercentage").value = ""; 
+    } else {
+        document.getElementById("totalBill").textContent = "Please enter valid numbers"; 
+    }    
+}
+
+const buttonTip = document.querySelector("#total-button"); 
+buttonTip.addEventListener("click", calculateTotal); 
