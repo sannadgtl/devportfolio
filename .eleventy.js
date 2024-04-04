dafb00b0d70d20f39420e61565ea3043a0cdc622
sans <sannadgtl@gmail.com>
2024-04-04 16:33:03 +0100

    started adding JS projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images/"); 
     eleventyConfig.addPassthroughCopy("./src/style.css"); 
+    eleventyConfig.addPassthroughCopy("./src/index.js"); 
     // eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
 
     return {
---
+++ src/index.js
@@ -0,0 +1,30 @@
+// drink order 
+function submitOrder() {
+    const flavour = document.querySelector('input[name="flavour"]:checked'); 
+    const sizeInput = document.querySelector('input[name="size"]:checked'); 
+
+    if (flavour && sizeInput) {
+        const size = sizeInput.value; 
+        switch (flavour.value) {
+            case "cola": 
+                document.querySelector("#order").textContent = `You have ordered a ${size} Cola`; 
+                break; 
+            case "lemon": 
+                document.querySelector("#order").textContent = `You have ordered a ${size} Lemonade`; 
+                break; 
+            case "orange": 
+                document.querySelector("#order").textContent = `You have ordered a ${size} Orangeade`; 
+                break;
+            default: 
+        }
+    } else if (flavour && !sizeInput) {
+        document.querySelector("#order").textContent = `Please choose a drink size`; 
+    } else if (!flavour && sizeInput) {
+        document.querySelector("#order").textContent = `Please choose a drink flavour`;
+    } else {
+        document.querySelector("#order").textContent = `Please choose a drink size and flavour`; 
+    }
+}
+
+const button = document.querySelector("#submit-btn");
+button.addEventListener("click", submitOrder);  
