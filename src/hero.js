b5baeb301a966d8b0c8114baa032efabfcd1cbd6
sans <sannadgtl@gmail.com>
2024-04-12 20:38:30 +0100

    cleaned up hero js

--- src/hero.js
+++ src/hero.js
@@ -3,17 +3,17 @@ const ul = document.querySelector("#hero");
 let counter = 0
 
 function addAnimation() {
-    const li = document.createElement("li"); 
-    li.innerText = texts[counter]; 
-    ul.appendChild(li); 
+  const li = document.createElement("li"); 
+  li.innerText = texts[counter]; 
+  ul.appendChild(li); 
 
-    li.classList.add("animated-item"); 
-    li.addEventListener('animationend', (e) => {
-        e.target.remove();
-        counter++;
-        if(counter == texts.length) counter = 0;
-        addAnimation();
-      });
-    }
-
+  li.classList.add("animated-item"); 
+  li.addEventListener("animationend", (e) => {
+    e.target.remove(); 
+    counter++; 
+    if (counter == texts.length) counter = 0; 
     addAnimation(); 
+  }); 
+}
+
+addAnimation(); 
